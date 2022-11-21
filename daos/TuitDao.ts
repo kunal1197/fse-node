ecb092fdb3c25ee8cbb46ac7f2bd32889507372f
Kunal Mehrotra <kunalmehrotra@Kunals-MacBook-Pro.local>
2022-11-21 06:03:22 -0500

    Added update likes functionality and made changes to accomodate the find all tuits fnction

--- daos/TuitDao.ts
+++ daos/TuitDao.ts
@@ -19,7 +19,6 @@ export default class TuitDao implements TuitDaoI {
     }
     return TuitDao.tuitDao;
   };
-
   private constructor() {}
 
   /**
@@ -27,8 +26,8 @@ export default class TuitDao implements TuitDaoI {
    * @returns Promise To be notified when the tuits are retrieved from
    * database
    */
-  findAllTuits = async (): Promise<Tuit[]> => TuitModel.find();
-
+  findAllTuits = async (): Promise<Tuit[]> =>
+    TuitModel.find().populate("postedBy").exec();
   /**
    * Uses TuitModel to retrieve all tuit documents created by a user
    * @param {string} uid User's primary key
@@ -36,7 +35,10 @@ export default class TuitDao implements TuitDaoI {
    * database
    */
   findAllTuitsByUser = async (uid: string): Promise<Tuit[]> =>
-    TuitModel.find({ postedBy: uid });
+    TuitModel.find({ postedBy: uid })
+      .sort({ postedOn: -1 })
+      .populate("postedBy")
+      .exec();
 
   /**
    * Uses TuitModel to retrieve a specific tuit document from the database
@@ -62,8 +64,8 @@ export default class TuitDao implements TuitDaoI {
    * @param {Tuit} tuit the body of the updated tuit
    * @returns Promise To be notified when the tuit has been updated
    */
-  updateTuit = async (uid: string, tuit: Tuit): Promise<any> =>
-    TuitModel.updateOne({ _id: uid }, { $set: tuit });
+  updateTuit = async (tid: string, tuit: Tuit): Promise<any> =>
+    TuitModel.updateOne({ _id: tid }, { $set: tuit });
 
   /**
    * Uses TuitModel to delete a tuit
@@ -72,4 +74,13 @@ export default class TuitDao implements TuitDaoI {
    */
   deleteTuit = async (uid: string): Promise<any> =>
     TuitModel.deleteOne({ _id: uid });
+
+  /**
+   * Uses TuitModel to retrieve all tuit documents from tuits collection
+   * @param {string} tid Tuit's primary key
+   * @returns Promise To be notified when the tuits are retrieved from
+   * database
+   */
+  updateLikes = async (tid: string, newStats: any): Promise<any> =>
+    TuitModel.updateOne({ _id: tid }, { $set: { stats: newStats } });
 }
