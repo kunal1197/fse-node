084d778ff258f9e2c4b1b45e8e5475371bd88159
Kunal Mehrotra <kunalmehrotra@Kunals-MacBook-Pro.local>
2022-11-29 00:12:03 -0500

    point urls

--- daos/DislikeDao.ts
+++ daos/DislikeDao.ts
@@ -1,6 +1,6 @@
 import DislikeDaoI from "../interfaces/DislikeDaoI";
 import DislikeModel from "../mongoose/dislikes/DislikeModel";
-import Dislike from "../models/dislikes/dislike";
+import Dislike from "../models/dislikes/Dislike";
 export default class DislikeDao implements DislikeDaoI {
   private static dislikeDao: DislikeDao | null = null;
   public static getInstance = (): DislikeDao => {
